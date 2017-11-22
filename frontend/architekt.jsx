300387d1e9b6332eb3d228a13b0338b109ef15d6
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-22 10:11:20 -0800

    Adds session_actions consisting of login, logout and signup

--- frontend/architekt.jsx
+++ frontend/architekt.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//FOR TESTING ... importing to test login, logout and signup requests
 import * as APIUtil from './util/session_api_util'
 
 
 document.addEventListener('DOMContentLoaded', () => {
+
+  //FOR TESTING
   window.login = APIUtil.login;
   window.logout = APIUtil.logout;
   window.signup = APIUtil.signup;
+  //FOR TESTING
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Welcome to Architekt</h1>, root);
 });
