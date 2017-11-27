3f1176bfba49a4b8b20232e2adbc00f4a8c25995
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-27 10:55:53 -0800

    Adds controller methods for index, update, show and destory for stories

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -4,27 +4,38 @@ class Api::StoriesController < ApplicationController
   end
 
   def create
-
-  end
-
-  def new
-
+    @story = Story.new(story_params)
+    @story.author_id = current_user.id
+    if @story.save
+      render :show
+    else
+      render json: @story.errors.full_messages, status: 422
+    end
   end
 
   def show
-    
+    @story = Story.find(params[:id])
+    render :show
   end
 
-  def delete
+  def destroy
+    @story = current_user.stories.find(params[:id])
 
   end
 
-  def edit
-
+  def destory
+    @story = current_user.stories.find(params[:id])
+    @story.destroy
+    render :show
   end
 
   def update
-
+    @story = current_user.stories.find(params[:id])
+    if @story.update(story_params)
+      render :show
+    else
+      render json: @story.errors.full_messages, status: 422
+    end
   end
 
   private
