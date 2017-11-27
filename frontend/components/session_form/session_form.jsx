e280dd49dc8bdc6d7e8568bd94a9719cbf28efe5
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-27 10:12:59 -0800

    Adds stylesheet for reset and session form, needs to fix the bug where closing the modal doesnt work

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -23,28 +23,47 @@ class SessionForm extends React.Component{
     };
   }
 
+  handleModal(e){
+    e.preventDefault();
+
+  }
+
   render(){
     let text = this.props.formType==="login"? "Sign in" : "Sign up";
     let buttonText = this.props.formType==="login"? "Sign In" : "Sign Up";
     return (
-      <div className="modal">
-        <div className="modal-screen"></div>
+      <div className="modal is-open">
+        <form onSubmit={this.handleSubmit} className="modal-form">
 
-        <div className="modal-form">
-        <Link className="x-button" to="/">X</Link>
-        <h2>{text}</h2>
-        <form onSubmit={this.handleSubmit} action="">
-          <label htmlFor="">
-            <input className="text-form" type="text" placeholder="Email" onChange={this.handleChange('email')} value={this.state.email}/>
-          </label>
+          <span className="modal-close js-modal-close">&times;</span>
+
+          <h1>Welcome</h1>
           <br/>
-          <label htmlFor="">
-            <input className="text-form" type="password" placeholder="Password" onChange={this.handleChange('password')} value={this.state.password}/>
-          </label>
-          <br/>
-          <input type="submit" className="submit-button" value={buttonText}/>
+
+          <div className="input">
+
+            <label htmlFor="form-email">Email</label>
+            <input
+              placeholder="Email"
+              type="text" id="form-email"
+              onChange={this.handleChange('email')}
+              value={this.state.email}/>
+          </div>
+
+          <div className="input">
+            <label htmlFor="form-password">Password</label>
+            <input
+              type="password"
+              id="form-password"
+              onChange={this.handleChange('password')}
+              value={this.state.password}/>
+          </div>
+
+          <div>
+            <input type="submit" className="submit" value={buttonText}/>
+          </div>
         </form>
-        </div>
+        <div className="modal-screen js-modal-close"></div>
       </div>
     );
 
@@ -52,95 +71,3 @@ class SessionForm extends React.Component{
 }
 
 export default withRouter(SessionForm);
-
-// import React from 'react';
-// import { Link, withRouter } from 'react-router-dom';
-//
-// class SessionForm extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       email: '',
-//       password: ''
-//     };
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   componentWillReceiveProps(nextProps) {
-//     if (nextProps.loggedIn) {
-//       this.props.history.push('/');
-//     }
-//   }
-//
-//   update(field) {
-//     return e => this.setState({
-//       [field]: e.currentTarget.value
-//     });
-//   }
-//
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     const user = this.state;
-//     this.props.processForm({user});
-//   }
-//
-//   navLink() {
-//     if (this.props.formType === 'login') {
-//       return <Link to="/signup">sign up instead</Link>;
-//     } else {
-//       return <Link to="/login">log in instead</Link>;
-//     }
-//   }
-//
-//   // renderErrors() {
-//   //   return(
-//   //     <ul>
-//   //       {this.props.errors.map((error, i) => (
-//   //         <li key={`error-${i}`}>
-//   //           {error}
-//   //         </li>
-//   //       ))}
-//   //     </ul>
-//   //   );
-//   // }
-//
-//   render() {
-//
-//     let errors;
-//     if (this.props.errors.session !== []) {
-//       errors = this.props.errors.session;
-//     }
-//     return (
-//       <div className="login-form-container">
-//         <form onSubmit={this.handleSubmit} className="login-form-box">
-//           Welcome to Architekt!
-//           <br/>
-//           Please {this.props.formType} or {this.navLink()}
-//           {this.errors}
-//           <div className="login-form">
-//             <br/>
-//             <label>Email:
-//               <input type="text"
-//                 value={this.state.email}
-//                 onChange={this.update('email')}
-//                 className="login-input"
-//               />
-//             </label>
-//             <br/>
-//             <label>Password:
-//               <input type="password"
-//                 value={this.state.password}
-//                 onChange={this.update('password')}
-//                 className="login-input"
-//               />
-//             </label>
-//             <br/>
-//             <input type="submit" value="Submit" />
-//           </div>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default withRouter(SessionForm);
