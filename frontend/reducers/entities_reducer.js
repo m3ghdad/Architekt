743346759222010c3d07b35c8de81f4c881b1716
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2017-11-27 16:04:49 -0800

    Adds entities reducer as a combineReducer and adds it to root_reducer to shape our state

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux';
+
+import stories from './stories_reducer';
+
+export default combineReducers({
+  stories,
+});
--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
-import errorsReducer from './errors_reducer';
-import sessionReducer from './session_reducer';
+import errors from './errors_reducer';
+import entities from './entities_reducer';
+import session from './session_reducer';
 
 export default combineReducers({
-  errors: errorsReducer,
-  session: sessionReducer
+  errors,
+  session,
+  entities,
 });
